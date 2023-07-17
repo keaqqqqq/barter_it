0dfc8c6a960d13e381d899c8174e187bf292dca3
keaqqqqq <103634501+keaqqqqq@users.noreply.github.com>
2023-07-18 00:42:29 +0800

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mybarterit_db";
+$username = "keaqqqqq_admin";
+$password = "************";
+$dbname = "keaqqqqq_barter_it";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
